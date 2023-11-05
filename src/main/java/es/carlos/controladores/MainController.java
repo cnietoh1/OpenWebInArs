2ef5ce4261055899ee45542ef95ccc1684f09c1e
cnietoh1 <carlosnieth7@gmail.com>
2023-11-05 13:00:38 +0100

    version 05/11/2023 13:00

--- src/main/java/es/carlos/controladores/MainController.java
+++ src/main/java/es/carlos/controladores/MainController.java
@@ -3,21 +3,23 @@ package es.carlos.controladores;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String welcome(@RequestParam("name") String nombre, Model model) {
+    public String welcome(@RequestParam(name = "name", required=false, defaultValue = "Mundo") String nombre, Model model) {
         //model.addAttribute("mensaje", "Hola a todos los alumnos de Openwebinars");
         model.addAttribute("nombre", nombre);
         return "index";
     }
 
-    @GetMapping("/saludo")
-    public String saludo(Model model) {
-        model.addAttribute("saludo", "Seguro que has visto otras plataformas con miles de cursos...");
+    @GetMapping("/saludo/{name}")
+    public String saludo(@PathVariable String name, Model model) {
+        //model.addAttribute("saludo", "Seguro que has visto otras plataformas con miles de cursos...");
+        model.addAttribute("saludo", "Hola " + name);
         return "saludo";
     }
 }
